49321733b7006c4b2e37b15f5d75303a0e731c25
Dustin Hurst <the_friendship_warrior@hotmail.com>
2015-02-14 20:01:18 -0700

    final change

--- CS 2450 Palindrome Project/main.cpp
+++ CS 2450 Palindrome Project/main.cpp
@@ -13,15 +13,15 @@
 
 using namespace std;
 
-class Palindrome{
+class Palindrome{ //class that will find and calculate the palindrome
     public:
     
-    void printStr(char *str, int low, int high){
+    void printStr(char *str, int low, int high){  //function to print the longest palindrome
         for(int i = low; i <= high; ++i )
             cout << str[i];
     }
     
-    int longestPalindrome(char *str){
+    int longestPalindrome(char *str){ //function to find the longest palindrome and tally up its length
         int maxLength = 1;
         int start = 0;
         int len = strlen(str);
@@ -29,7 +29,7 @@ class Palindrome{
         for(int i = 0; i < len; ++i){
             low = i -1;
             high = i + 1;
-            while (low >= 0 && high < len && str[low] == str[high]){
+            while (low >= 0 && high < len && str[low] == str[high]){ //assuming the palindrome will be odd
                 if(high - low + 1 > maxLength){
                     start = low;
                     maxLength = high - low + 1;
@@ -46,6 +46,29 @@ class Palindrome{
 };
 
 int main(int argc, const char * argv[]) {
+    
+    ifstream inFile;
+    
+    cout << "Please input file name: " << flush; //read an input file
+    while(true){
+        string inFileName;
+        getline(cin, inFileName);
+        inFile.open(inFileName.c_str());
+        if(inFile)break;
+        cout << "Invalide entry. Please enter file name: " << flush;
+    }
+    
+    char str[1000];                 //assuming the file has less than 1000 chars
+    for(int i = 0; i < 1000; ++i){
+        inFile >> str[i];
+        
+    }
+    
+    inFile.close();
+    
+    Palindrome pal;
+    printf("\nLength is: %d",pal.longestPalindrome(str)); //output is just to the console
+    cout << endl;
     
     return 0;
 }
